ab49cfb85b1c8e62f59c8118a4689b41d8ac9e20
arthur-mhts <arthur.mehats@gmail.com>
2024-04-12 15:25:03 +0200

    function test

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,11 @@
-from calculator import addition
-
+from calculator import addition, soustraction, division
 
 
 def test_addition():
     assert addition(1, 2) == 3
+
+def test_soustraction():
+	assert soustraction(2,1) == 1
+
+def test_division():
+	assert division(6,2) == 3
